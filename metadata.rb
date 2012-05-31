677cdae437726293575e84e397fc60f7b87d579f
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-05-31 14:40:00 +0000

    Fix rbenv recipe so environment is properly setup after fist install. This allows passenger to compile within nginx on first install.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "nick.muerdter@nrel.gov"
 license          "All rights reserved"
 description      "Installs/Configures awstats"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends "apache2"
 depends "logrotate"
