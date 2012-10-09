NREL-cookbooks/bundler

Adding dependencies.

metadata.rb
@@ -2,3 +2,5 @@ maintainer        "37signals"
 maintainer_email  "sysadmins@37signals.com"
 description       "Configures gem bundler"
 version           "0.1"
+
+depends "ruby_enterprise"

NREL-cookbooks/awstats

metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures awstats"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+depends "apache2"
+depends "logrotate"
+depends "rsync"
36bf2d6a1a7e40646ba66d236b1b1d2ca02602d6
Muerdter, Nick <nick.muerdter@nrel.gov>
2010-12-03 00:38:51 +0000

    Adding dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures awstats"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+depends "apache2"
+depends "logrotate"
+depends "rsync"
