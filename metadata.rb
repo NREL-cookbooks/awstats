a50346e466bbcce99c8991f604fbfcb4764e191d
Nick Muerdter <nick.muerdter@nrel.gov>
2012-10-09 18:03:44 -0600

    Fix readme path.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "NREL"
 maintainer_email "nick.muerdter@nrel.gov"
 license          "All rights reserved"
 description      "Installs/Configures awstats"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
 
 depends "logrotate"
