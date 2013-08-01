fcaea397b6eb9659c9582182c2c0f10fbc5992ff
Nick Muerdter <nick.muerdter@nrel.gov>
2013-08-01 17:32:11 -0600

    Bump awstats to 7.2 and fix download URL.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:awstats][:version] = "7.0"
-default[:awstats][:url] = "http://voxel.dl.sourceforge.net/project/awstats/AWStats/#{awstats[:version]}/awstats-#{awstats[:version]}.tar.gz"
-default[:awstats][:archive_checksum] = "e2651d4a5ff40e1d792b37cc13784d444cae479b4f98118aed27f46f5a18ce92"
+default[:awstats][:version] = "7.2"
+default[:awstats][:url] = "http://superb-dca2.dl.sourceforge.net/project/awstats/AWStats/#{awstats[:version]}/awstats-#{awstats[:version]}.tar.gz"
+default[:awstats][:archive_checksum] = "740cb46ac5a7befb72ff0b80d0e53c6fe3560700208bce18f4fb5b2b5e275791"
 default[:awstats][:install_path] = "/opt/awstats"
 default[:awstats][:log_file] = "/var/log/httpd/mylog.log"
 default[:awstats][:data_path] = "/var/lib/awstats"
